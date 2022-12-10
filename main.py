d9493eade470362fc8c7e5cb96aef67e99b31af5
hang-yin <yinhang0226@gmail.com>
2022-12-10 17:28:18 -0600

    Added user input for selecting which part to run

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def main():
                         mouse_sensitivity=0.003,
                         color_limit_low="0x00005F",
                         color_limit_high="0x0000FF")
-    graphics.plot('part2')
+    # ask for user input to know which part to run
+    part = input("Which part do you want to run? (1 or 2) ")
+    if part == '1':
+        graphics.plot('part1')
+    elif part == '2':
+        graphics.plot('part2')
+    else:
+        raise ValueError("Part must be 1 or 2")
 
 if __name__ == '__main__':
     main()
